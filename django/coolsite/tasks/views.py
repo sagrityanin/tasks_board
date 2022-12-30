186775ccc925eaf9b351d7d4aedca65bf8f1c5e5
andrey <sagrityanin@yandex.ru>
2022-12-30 06:39:31 +0500

    login

--- django/coolsite/tasks/views.py
+++ django/coolsite/tasks/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 from django import forms
@@ -6,24 +8,20 @@ from .forms import AddTaskForm, EditTaskForm
 from django.shortcuts import get_object_or_404
 from django.contrib import messages
 
-menu = [{'title': "О сайте", 'url_name': '/about'},
-        {'title': "Добавить задачу", 'url_name': '/new-task'},
-        {'title': "Список задач по категориям", 'url_name': '/tasks/all'},
-        {'title': "Выйти", 'url_name': '/logout'}]
+menu = [{"title": "О сайте", "url_name": "/about"},
+        {"title": "Добавить задачу", "url_name": "/new-task"},
+        {"title": "Список задач по категориям", "url_name": "/tasks/all"},
+        {"title": "Войти", "url_name": "/login"},
+        {"title": "Выйти", "url_name": "/logout"}]
 status = {"created": "Активные задачи", "executed": "Выполненые задачи",
           "deprecated": "Отклоненные задачи", "all": "Все задачи"}
 
 
-# class TaskForm(forms.ModelForm):
-#     class Meta:
-#         model = Task
-#         fields = ("title", 'note', 'is_visible', 'status', 'creator', 'executor')
-
-
 def index(request):
-    return render(request, 'tasks/index.html', {'menu': menu, 'title': 'Главная страница'})
+    return render(request, "tasks/index.html", {"menu": menu, "title": "Главная страница"})
 
 
+@login_required(login_url="/about/")
 def tasks(request, category):
     if category == "all":
         tasks = Task.objects.all().order_by("-time_updated")
@@ -32,49 +30,50 @@ def tasks(request, category):
     else:
         tasks = Task.objects.filter(status=category).order_by("-time_updated")
     context = {
-        'tasks': tasks,
-        'menu': menu,
-        'title': status[category]
+        "tasks": tasks,
+        "menu": menu,
+        "title": status[category]
     }
-    return render(request, 'tasks/tasks.html', context=context)
+    return render(request, "tasks/tasks.html", context=context)
 
 
+@login_required(login_url="/about/")
+@transaction.atomic
 def edit_task(request, task_id):
-    if request.method == 'POST':
+    if request.method == "POST":
         instance = Task.objects.get(pk=task_id)
         form = EditTaskForm(request.POST, instance=instance)
         if form.is_valid():
             Task.creator = Task.objects.get(pk=task_id).creator
             form.save()
-            return redirect('tasks')
+            return redirect("tasks")
     else:
         task = get_object_or_404(Task, pk=task_id)
         form = EditTaskForm(instance=task)
         task_link = f"/task/{task_id}"
-    return render(request, 'tasks/edit_task.html', {'form': form, 'menu': menu, 'title': 'Изменение задачи',
-                                                   'created': task.time_created,
-                                                   'updated': task.time_updated, "creator": task.creator.user.username,
-                                                    'task_link': task_link})
+    return render(request, "tasks/edit_task.html", {"form": form, "menu": menu, "title": "Изменение задачи",
+                                                   "created": task.time_created,
+                                                   "updated": task.time_updated, "creator": task.creator.user.username,
+                                                    "task_link": task_link})
 
 
+@login_required(login_url="/about/")
+@transaction.atomic
 def new_task(request):
-    if request.method == 'POST':
+    print(request.user)
+    if request.method == "POST":
         form = AddTaskForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('tasks')
+            return redirect("tasks")
     else:
         form = AddTaskForm()
-    return render(request, 'tasks/new_task.html', {'form': form, 'menu': menu, 'title': 'Добавление задачи'})
+    return render(request, "tasks/new_task.html", {"form": form, "menu": menu, "title": "Добавление задачи"})
 
 
 def about(request):
-    return render(request, 'tasks/about.html', {'menu': menu, 'title': 'О сайте'})
-
-
-def logout(request):
-    return HttpResponse("Здесь будет logout")
+    return render(request, "tasks/about.html", {"menu": menu, "title": "О сайте"})
 
 
 def pageNotFound(request, exception):
-    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
