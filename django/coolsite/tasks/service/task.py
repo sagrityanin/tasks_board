d456a5c572c109f9cc74ac76b7fa5b55b46819b3
andrey <sagrityanin@yandex.ru>
2022-12-30 11:03:19 +0500

    invisivle tasks

---
+++ django/coolsite/tasks/service/task.py
@@ -0,0 +1,14 @@
+from django.db.models import Q
+
+from tasks.models import Task, User
+
+
+def get_tasks(request, category):
+    if category == "all":
+        tasks = Task.objects.filter(Q(creator=request.user.person) | Q(executor=request.user.person) |
+                                    Q(is_visible=True)).order_by("-time_updated")
+    else:
+        tasks = Task.objects.filter(Q(creator=request.user.person) | Q(executor=request.user.person) |
+                                    Q(is_visible=True)).filter(status=category).order_by("-time_updated")
+
+    return tasks
