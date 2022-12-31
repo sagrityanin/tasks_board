9aa6301b5f771c7c65bb21c6302fc34d1f9c621a
andrey <sagrityanin@yandex.ru>
2022-12-31 15:00:14 +0500

    selenium change task status

--- tests/change_status_tasks.py
+++ tests/change_status_tasks.py
@@ -9,38 +9,40 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from dotenv import load_dotenv
 
-count = 100
-url = "https://task.info66.ru:4443"
 url_login = "https://task.info66.ru:4443/login/"
 
 load_dotenv()
 
 
-def make_task(browser, i: int) -> None:
-    browser.find_element(By.ID, "id_title").send_keys(f"Тестовая задача {i}")
-    browser.find_element(By.ID, "id_executor").find_elements(By.TAG_NAME, "option")[1].click()
+def change_status(browser) -> None:
+    browser.find_element(By.ID, "id_status").find_elements(By.TAG_NAME, "option")[1].click()
     sleep(1)
-    browser.find_element(By.XPATH, '//button[@type="submit"]').click()
-    sleep(1)
+    browser.find_element(By.XPATH, "//*[contains(text(), 'Сохранить')]").click()
 
 
-def login(browser) -> None:
+def login(browser):
     browser.get(url_login)
     WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
         (By.ID, "id_username"))).send_keys(os.getenv("TASK_USER"))
     browser.find_element(By.ID, "id_password").send_keys(os.getenv("TASK_PASSWORD"))
     browser.find_element(By.TAG_NAME, "button").click()
-    sleep(1)
+    sleep(2)
 
 
 def main(count: int) -> None:
     with webdriver.Chrome() as browser:
         login(browser)
         for i in range(count):
-            browser.find_element(By.XPATH, "//*[contains(text(), 'Добавить задачу')]").click()
-            print(f"Создание задачи {i}")
+            browser.get("https://task.info66.ru:4443/tasks/%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D0%BD%D0%B0/")
             sleep(1)
-            make_task(browser, i)
+            list_tasks = browser.find_elements(By.XPATH, "//*[contains(text(), 'Детали задачи')]")
+            if len(list_tasks) > 0:
+                list_tasks[0].click()
+                print(f"Меняем статус задачи {i}")
+                change_status(browser)
+            else:
+                print("Все задачи обработаны")
+                break
 
 
 if __name__ == "__main__":
