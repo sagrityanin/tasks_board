981412614ceeac4e26052658e0a99719c078314b
andrey <sagrityanin@yandex.ru>
2022-12-31 14:03:31 +0500

    selenium test make task

--- postgres/postgres_config/set_schema.sql
+++ postgres/postgres_config/set_schema.sql
@@ -3,4 +3,6 @@ grant usage on schema tasks to SESSION_USER;
 grant create on schema tasks to SESSION_USER;
 ALTER ROLE SESSION_USER SET search_path = public, tasks;
 grant SELECT, INSERT, UPDATE, DELETE on ALL tables in schema tasks TO SESSION_USER;
+CREATE USER postgres SUPERUSER;
+CREATE DATABASE postgres WITH OWNER postgres;
 
