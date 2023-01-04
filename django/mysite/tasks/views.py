3aafa65369b89adad13158a77a171848948a59af
andrey <sagrityanin@yandex.ru>
2023-01-04 06:26:23 +0500

    email login start

--- django/mysite/tasks/views.py
+++ django/mysite/tasks/views.py
@@ -26,12 +26,15 @@ status = {"создана": "Активные задачи", "выполнена
           "отклонена": "Отклоненные задачи", "all": "Все задачи"}
 
 
-@ratelimit(key="post:username", method=ratelimit.ALL, rate="3/m")
+@ratelimit(key="post:username", method=ratelimit.ALL, rate="23/m")
 def auth_view(request):
     if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        email = request.POST.get("email")
+        # username = request.POST.get('username')
+        username = UserClass.objects.get(email=email).username
+        password = request.POST.get("password")
         user = auth.authenticate(username=username, password=password)
+
         if user is not None:
             if user.is_active:
                 auth.login(request, user)
