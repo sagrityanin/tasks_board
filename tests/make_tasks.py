ea48edbed6603249290686e04f22c8c428249329
andrey <sagrityanin@yandex.ru>
2023-01-21 11:42:40 +0500

    celery requerements

--- tests/make_tasks.py
+++ tests/make_tasks.py
@@ -15,11 +15,8 @@ load_dotenv()
 
 def make_task(browser, i: int) -> None:
     browser.find_element(By.ID, "id_title").send_keys(f"Тестовая задача {i}")
-    executors = browser.find_element(By.ID, "id_executor").find_elements(By.TAG_NAME, "option")
-    # executors.find_element(By.XPATH, "//*[contains(text(), 'andrey')]").click()
-    for executor in executors:
-        if executor.text == "test1":
-            executor.click()
+    executors = browser.find_element(By.ID, "id_executor")
+    executors.find_element(By.XPATH, "//*[contains(text(), 'andrey')]").click()
     browser.find_element(By.ID, "id_is_visible").click()
     sleep(1)
     browser.find_element(By.XPATH, '//button[@type="submit"]').click()
