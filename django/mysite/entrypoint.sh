df52a961bcf4b10a1a4b09caa89514b916941f2a
Andrey Ivanov <andrey.i@reviewapp.today>
2024-03-22 16:49:11 +0500

    init

--- django/mysite/entrypoint.sh
+++ django/mysite/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-python manage.py makemigrations
-python manage.py makemigrations tasks
-python manage.py migrate
-
 echo "Start app"
 exec gunicorn coolsite.wsgi --bind 0.0.0.0:8000 --workers 2
 echo "App started"
