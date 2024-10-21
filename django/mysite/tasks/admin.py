9a17a626af2bc79cfb0a0fdbb6c110aa8dd17781
Andrey Ivanov <andrey.i@reviewapp.today>
2024-10-21 07:41:46 +0500

    task section model

--- django/mysite/tasks/admin.py
+++ django/mysite/tasks/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Person, Task, StatusModel, Pc
+from .models import Person, Task, StatusModel, Pc, TaskSection
 
 admin.site.register(Person)
 admin.site.register(Task)
 admin.site.register(StatusModel)
+admin.site.register(TaskSection)
 @admin.register(Pc)
 class PcAdmin(admin.ModelAdmin):
     list_display = ("title", "telefon_number", "ip", "rdb_user", "note")
